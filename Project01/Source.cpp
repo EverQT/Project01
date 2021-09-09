2c9a1117bb8c3343cbcd54c844256a3d17fcedc1
SAMSUNG <SAMSUNG@DESKTOP-ANNUGID>
2021-09-08 20:53:35 -0400

    Agregar archivos de proyecto.

---
+++ Project01/Source.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << " Ever Quispe Torrez " << endl;
+	cout << " CI: 10463104 " << endl;
+	cout << " CU: 35-4504 " << endl;
+	cout << " Ing. de Sistemas " << endl;
+}
